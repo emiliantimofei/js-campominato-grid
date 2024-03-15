84acf64f6e05c6c2b1378eef2417b49d38da5911
emiliantimofei <metimo1996@gmail.com>
2024-03-15 15:56:06 +0100

    grid appears now on click of button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+// spesa: bottone + app
 let bottone = document.getElementById('bottone');
 
-document.get
+let app = document.getElementById('app');
+
+// cliccando il bottone mi appare la griglia
+document.addEventListener('click', function(){
+    app.classList.replace('d-n', 'd-b');
+});
+
+// spesa: mini box child
+let child = document.querySelector('child');
+
+document.addEventListener('click', function(){
+   child.classList.add('bg-click');
+});
+
